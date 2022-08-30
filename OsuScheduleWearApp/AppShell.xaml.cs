6b1731828067970a591dccdee6d29dd394492c2c
XDenizX <denizik1@mail.ru>
2022-08-30 13:52:15 +0300

    Добавьте файлы проекта.

---
+++ OsuScheduleWearApp/AppShell.xaml.cs
@@ -0,0 +1,9 @@
+﻿namespace OsuScheduleWearApp;
+
+public partial class AppShell : Shell
+{
+	public AppShell()
+	{
+		InitializeComponent();
+	}
+}
---
+++ OsuScheduleWearApp/MainPage.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace OsuScheduleWearApp;
+
+public partial class MainPage : ContentPage
+{
+	public MainPage()
+	{
+		InitializeComponent();
+	}
+}
+
