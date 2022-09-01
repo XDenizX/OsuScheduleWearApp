aa188675b411892b94e10e210d9f5a54b2b64590
XDenizX <denizik1@mail.ru>
2022-09-01 22:56:04 +0300

    Synchronized namespaces.

--- OsuScheduleWearApp/Platforms/MacCatalyst/AppDelegate.cs
+++ OsuScheduleWearApp/Platforms/MacCatalyst/AppDelegate.cs
@@ -1,9 +1,9 @@
 ﻿using Foundation;
 
-namespace OsuScheduleWearApp;
+namespace OsuScheduleWearApp.Platforms.MacCatalyst;
 
 [Register("AppDelegate")]
 public class AppDelegate : MauiUIApplicationDelegate
 {
-	protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+    protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
 }
