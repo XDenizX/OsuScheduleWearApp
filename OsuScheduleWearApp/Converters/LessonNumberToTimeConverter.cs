f4108dabd463b7bd9b6f7d98ac0a66d607057c2e
XDenizX <denizik1@mail.ru>
2022-09-03 19:18:14 +0300

    Added LessonNumberToTime and StringToDate converters.

---
+++ OsuScheduleWearApp/Converters/LessonNumberToTimeConverter.cs
@@ -0,0 +1,28 @@
+﻿using OsuScheduleWearApp.Helpers;
+using System.Globalization;
+
+namespace OsuScheduleWearApp.Converters;
+
+internal class LessonNumberToTimeConverter : IValueConverter
+{
+
+    public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+    {
+        if (value is not int)
+        {
+            throw new ArgumentException(nameof(value));
+        }
+
+        var lessonNumber = (int) value;
+
+        TimeSpan startTime = LessonTimeHelper.GetStartTime(lessonNumber);
+        TimeSpan endTime = LessonTimeHelper.GetEndTime(lessonNumber);
+
+        return $"{startTime:hh\\:mm} - {endTime:hh\\:mm}";
+    }
+
+    public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+    {
+        throw new NotImplementedException();
+    }
+}
---
+++ OsuScheduleWearApp/Converters/StringToDateTimeConverter.cs
@@ -0,0 +1,23 @@
+﻿using System.Globalization;
+
+namespace OsuScheduleWearApp.Converters;
+
+internal class StringToDateTimeConverter : IValueConverter
+{
+    public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+    {
+        if (value is not string)
+        {
+            throw new ArgumentException(nameof(value));
+        }
+
+        var dateText = value as string;
+
+        return DateTime.Parse(dateText);
+    }
+
+    public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+    {
+        throw new NotImplementedException();
+    }
+}
