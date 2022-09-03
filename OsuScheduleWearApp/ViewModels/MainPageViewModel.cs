e29c85adbdc00c523e088210c3cc42b01a096d79
XDenizX <denizik1@mail.ru>
2022-09-03 19:19:30 +0300

    Created view model for the main page.

---
+++ OsuScheduleWearApp/ViewModels/MainPageViewModel.cs
@@ -0,0 +1,48 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
+using OrelStateUniversity.API;
+using OrelStateUniversity.API.Models;
+
+namespace OsuScheduleWearApp.ViewModels;
+
+partial class MainPageViewModel : ObservableObject
+{
+    [ObservableProperty]
+    private Schedule _schedule;
+
+    partial void OnScheduleChanged(Schedule value)
+    {
+        Lesson = GetActualLesson(value);
+    }
+
+    [ObservableProperty]
+    private Lesson _lesson;
+
+    [ObservableProperty]
+    private AsyncRelayCommand _updateScheduleCommand;
+
+    private readonly ScheduleApiClient _client;
+
+    public MainPageViewModel()
+    {
+        _client = new ScheduleApiClient();
+
+        UpdateScheduleCommand = new AsyncRelayCommand(UpdateScheduleAsync);
+        UpdateScheduleCommand.Execute(this);
+    }
+
+    private Lesson GetActualLesson(Schedule schedule)
+    {
+        return schedule.Lessons
+            .GroupBy(lesson => lesson.Date)
+            .OrderBy(lessonsGroup => lessonsGroup.Key)
+            .FirstOrDefault()
+            .OrderBy(lesson => lesson.Number)
+            .FirstOrDefault();
+    }
+
+    private async Task UpdateScheduleAsync()
+    {
+        Schedule = await _client.GetScheduleAsync(8379);
+    }
+}
