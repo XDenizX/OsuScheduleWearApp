a710378906143da8c10e0dd93d487521f581bf4e
XDenizX <denizik1@mail.ru>
2022-09-03 19:19:03 +0300

    Usage a CommunityToolkit.

--- OsuScheduleWearApp/MauiProgram.cs
+++ OsuScheduleWearApp/MauiProgram.cs
@@ -1,4 +1,6 @@
-﻿namespace OsuScheduleWearApp;
+﻿using CommunityToolkit.Maui;
+
+namespace OsuScheduleWearApp;
 
 public static class MauiProgram
 {
@@ -7,6 +9,7 @@ public static class MauiProgram
 		var builder = MauiApp.CreateBuilder();
 		builder
 			.UseMauiApp<App>()
+			.UseMauiCommunityToolkit()
 			.ConfigureFonts(fonts =>
 			{
 				fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular");
